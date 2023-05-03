74e66e00d199ab1bbf5c0c4eac31d8c9c0257bfe
Francisco Aguirre <franmaguirre@gmail.com>
2023-05-03 15:21:06 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+// import './App.css';
+import { Button } from "@mui/material";
+
+function App() {
+  return (
+    <div>
+      <Button>Este es un botón</Button>
+
+    </div>
+  );
+}
+
+export default App;
